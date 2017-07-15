3f812fa21567c9d70de266ec88da06c5fd94c7b2
Pascal Hertleif <killercup@gmail.com>
2017-07-15 18:53:07 +0200

    Poor man's compile-test

--- tests/it_builds.rs
+++ tests/it_builds.rs
@@ -16,15 +16,14 @@ fn can_serialize() {
     assert_eq!(st, vec![0, 0, 0, 0]);
 }
 
-// TODO: figure out some way of testing this without spinning up a DB.
-// #[test]
-// fn can_deserialize() {
-//     use diesel::types::FromSql;
-//     use diesel::backend::Backend;
-//     let mut st: [u8; 4] = [0, 0, 0, 0];
-//     let mut val = <diesel::sqlite::Sqlite as Backend>::RawValue(&st);
-//     let myid =
-//         <MyId as FromSql<diesel::types::Integer, diesel::sqlite::Sqlite>>::from_sql(Some(&st))
-//         .unwrap();
-//     assert_eq!(myid, MyId(0));
-// }
+#[allow(dead_code)]
+fn can_deserialize() {
+    use diesel::types::FromSql;
+    use diesel::backend::Backend;
+    let mut st: [u8; 4] = [0, 0, 0, 0];
+    let mut val = <diesel::sqlite::Sqlite as Backend>::RawValue(&st);
+    let myid =
+        <MyId as FromSql<diesel::types::Integer, diesel::sqlite::Sqlite>>::from_sql(Some(&st))
+        .unwrap();
+    assert_eq!(myid, MyId(0));
+}
