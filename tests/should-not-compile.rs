729dc14dd9d5244525bbcb17e441fa9894c7c8e4
Oliver Cooper <oliver.cooper@me.com>
2021-05-26 11:35:00 +1200

    Update diesel

--- tests/should-not-compile.rs
+++ tests/should-not-compile.rs
@@ -1,11 +1,13 @@
 //! This is a test file that *DOES* compile and pass tests, but which should
 //! not
 
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_derive_newtype;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_derive_newtype;
 
-use diesel::prelude::*;
 use diesel::dsl::sql;
+use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, DieselNewType)]
@@ -14,9 +16,8 @@ pub struct MyId(String);
 #[derive(Debug, Clone, PartialEq, Eq, Hash, DieselNewType)]
 pub struct OtherId(String);
 
-
 #[derive(Debug, Clone, PartialEq, Identifiable, Insertable, Queryable)]
-#[table_name="my_entities"]
+#[table_name = "my_entities"]
 pub struct MyEntity {
     id: MyId,
     val: i32,
@@ -31,28 +32,38 @@ table! {
 
 #[cfg(test)]
 fn setup() -> SqliteConnection {
-    let conn = SqliteConnection::establish(":memory:").unwrap();
+    let mut conn = SqliteConnection::establish(":memory:").unwrap();
     let setup = sql::<diesel::sql_types::Bool>(
         "CREATE TABLE IF NOT EXISTS my_entities (
                 id TEXT PRIMARY KEY,
                 val Int
-         )");
-    setup.execute(&conn).expect("Can't create table");
+         )",
+    );
+    setup.execute(&mut conn).expect("Can't create table");
     conn
 }
 
 #[cfg(test)]
 fn setup_with_items() -> (SqliteConnection, Vec<MyEntity>) {
-    let conn = setup();
+    let mut conn = setup();
     let objs = vec![
-        MyEntity { id: MyId("loop".into()), val: 0 },
-        MyEntity { id: MyId("WooHoo".into()), val: 1 },
-        MyEntity { id: MyId("boo".into()), val: 2 },
+        MyEntity {
+            id: MyId("loop".into()),
+            val: 0,
+        },
+        MyEntity {
+            id: MyId("WooHoo".into()),
+            val: 1,
+        },
+        MyEntity {
+            id: MyId("boo".into()),
+            val: 2,
+        },
     ];
 
     diesel::insert_into(my_entities::table)
         .values(&objs)
-        .execute(&conn)
+        .execute(&mut conn)
         .expect("Couldn't insert struct into my_entities");
 
     (conn, objs)
@@ -60,23 +71,23 @@ fn setup_with_items() -> (SqliteConnection, Vec<MyEntity>) {
 
 #[test]
 fn query_as_id() {
-    let (conn, _) = setup_with_items();
+    let (mut conn, _) = setup_with_items();
 
     let _: Vec<MyEntity> = my_entities::table
         .filter(my_entities::id.eq(OtherId("WooHoo".into()))) // <-- OTHERID
-        .load(&conn)
+        .load(&mut conn)
         .unwrap();
 }
 
 #[test]
 fn set() {
-    let (conn, objs) = setup_with_items();
+    let (mut conn, objs) = setup_with_items();
 
     let expected = objs[1].clone();
 
-    let new_id = OtherId("Oh My".into());  // <-- OTHERID
+    let new_id = OtherId("Oh My".into()); // <-- OTHERID
     diesel::update(my_entities::table.find(&expected.id))
         .set(my_entities::id.eq(&new_id))
-        .execute(&conn)
+        .execute(&mut conn)
         .unwrap();
 }
